04ec2cfc9f124eefc6a9b165dcf995fd5f845590
Robert Mastalerek <robert.mastalerek@gmail.com>
2025-06-17 13:53:36 +0200

    chore: tomcat distribution added as an option in the pipeline

--- release.sh
+++ release.sh
@@ -21,11 +21,11 @@ function build_and_push {
       printf -- "- $IMAGE:%s\n" "${tags[@]}" >> $GITHUB_STEP_SUMMARY
 }
 
-# check whether the CE image for distro was already released and exit in that case
-if [ $(docker manifest inspect $IMAGE:${DISTRO}-${VERSION} > /dev/null ; echo $?) == '0' ]; then
-    echo "Not pushing already released CE image"
-    exit 0
-fi
+## check whether the CE image for distro was already released and exit in that case
+#if [ $(docker manifest inspect $IMAGE:${DISTRO}-${VERSION} > /dev/null ; echo $?) == '0' ]; then
+#    echo "Not pushing already released CE image"
+#    exit 0
+#fi
 
 echo "${GHCR_PASSWORD}" | docker login ghcr.io -u "${GHCR_USERNAME}" --password-stdin
 
